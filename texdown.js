b7919d786df86694e2186653242bf9428d7d27ef
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-16 14:49:28 -0600

    switched to <img> instead of []()

--- texdown.js
+++ texdown.js
@@ -35,7 +35,8 @@ module.exports = function(markdown, options) {
           result.emit("error", err);
           return;
         }
-        otoks[idx] = '![alt text](data:image/png;base64,'+data.toString("base64") + ' "' + md_escape(itoks[idx]) +'")';
+        var escaped = md_escape(itoks[idx]);
+        otoks[idx] = ['<img src="data:image/png;base64,', data.toString("base64"), '">'].join('');
         if(--nspawned === 0) {
           result.end(otoks.join(""));
         }
